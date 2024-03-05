05ea682ffff77bdb4ef12d12a44c04a2b694a9e1
ysshim99 <91107424+ysshim99@users.noreply.github.com>
2024-03-06 00:44:13 +0900

    [Bronze II] Title: 카드 역배치, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10804. 카드 역배치/카드 역배치.cc
+++ 백준/Bronze/10804. 카드 역배치/카드 역배치.cc
@@ -3,13 +3,7 @@ using namespace std;
 
 int num[21];
 
-void reverse(int a, int b) {
-    for(int i = 0; i < (b-a+1)/2; ++i) {
-        swap(num[a+i], num[b-i]);
-    }
-}
-
-int main(void) {
+int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
     
@@ -21,7 +15,7 @@ int main(void) {
         int a, b;
         cin >> a >> b;
         
-        reverse(a, b);
+        reverse(num+a, num+b+1);
     }
     
     for(int i = 1; i < 21; ++i) {
