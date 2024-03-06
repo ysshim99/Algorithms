1326e21c40a6b5a6a14340bb23f75051000dd5e4
ysshim99 <91107424+ysshim99@users.noreply.github.com>
2024-03-06 17:30:56 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int T;
+    cin >> T;
+    
+    for(int i = 1; i <= T; ++i) {
+        for(int j = 0; j < T - i; ++j)
+            cout << " ";
+        for(int j = 0; j < i; ++j)
+            cout << "*";
+        cout << '\n';
+    }
+}
