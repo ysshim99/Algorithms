569d0be7540d5f495f66d00060a1c67eb667df74
ysshim99 <91107424+ysshim99@users.noreply.github.com>
2024-03-08 18:11:07 +0900

    [Silver IV] Title: 큐 2, Time: 400 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int N;
+    cin >> N;
+    
+    queue<int> Q;
+    while(N--) {
+        string op;
+        cin >> op;
+        
+        if(op == "push") {
+            int num;
+            cin >> num;
+            
+            Q.push(num);
+        }
+        else if(op == "pop") {
+            if(Q.empty()) cout << -1 << '\n';
+            else {
+                cout << Q.front() << '\n';
+                Q.pop();
+            }
+        }
+        else if(op == "size") {
+            cout << Q.size() << '\n';
+        }
+        else if(op == "empty") {
+            cout << Q.empty() << '\n';
+        }
+        else if(op == "front") {
+            if(Q.empty()) cout << -1 << '\n';
+            else cout << Q.front() << '\n';
+        }
+        else if(op == "back") {
+            if(Q.empty()) cout << -1 << '\n';
+            else cout << Q.back() << '\n';
+        }
+    }
+}
