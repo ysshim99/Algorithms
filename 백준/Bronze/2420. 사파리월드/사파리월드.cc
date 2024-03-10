33abb4ca2bdf7205d3febc8a21275ad5e86f9220
ysshim99 <91107424+ysshim99@users.noreply.github.com>
2024-03-11 05:48:59 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    long long N, M;
+    cin >> N >> M;
+    
+    cout << abs(N-M);
+}
