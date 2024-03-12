2629962cb5f4d057135f842220115933bbbe7c06
ysshim99 <91107424+ysshim99@users.noreply.github.com>
2024-03-12 21:54:18 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2200 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// pair.first(second) -> pair.X(Y) 로 사용
-#define X first
-#define Y second
+// pair.first(second) -> pair.Y(X) 로 사용
+#define Y first
+#define X second
 
 /*
 시작점인 (0, 0)을 제일 먼저 queue에 push
@@ -19,43 +19,41 @@ queue가 비어있기 전까지 계속 수행
 dist : -1로 초기화하여 해당 원소를 방문했는지 여부도 같이 알 수 있다.
 */
 
-string board[100];
-int dist[100][100];
+string board[102];
+int dist[102][102];
 int dx[4] = {0, 0, 1, -1};
 int dy[4] = {1, -1, 0, 0};
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     int N, M;
     cin >> N >> M;
-    
-    for(int i = 0; i < N; ++i)
+
+    for(int i = 0; i < N; ++i) {
         cin >> board[i];
-    
-    for(int i = 0; i < N; ++i) 
-        fill(dist[i], dist[i]+M, -1);
-    
+    }
+
     queue<pair<int, int> > Q;
     Q.push({0, 0});
-    dist[0][0] = 0;
-    
+    dist[0][0] = 1;
+
     while(!Q.empty()) {
         auto cur = Q.front();
         Q.pop();
-        
+
         for(int dir = 0; dir < 4; ++dir) {
-            int nx = cur.X + dx[dir];
             int ny = cur.Y + dy[dir];
-            
-            if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-            if(board[nx][ny] != '1' || dist[nx][ny] >= 0) continue;
-            
-            Q.push({nx, ny});
-            dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+            int nx = cur.X + dx[dir];
+
+            if(ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
+            if(board[ny][nx] == '0' || dist[ny][nx] > 0) continue;
+
+            dist[ny][nx] = dist[cur.Y][cur.X] + 1;
+            Q.push({ny, nx});
         }
     }
-    
-    cout << dist[N-1][M-1] + 1;
+
+    cout << dist[N-1][M-1];
 }
