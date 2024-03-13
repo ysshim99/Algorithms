000ad6caf178ac885f229b74067c61123d9273d8
ysshim99 <91107424+ysshim99@users.noreply.github.com>
2024-03-13 12:48:25 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    
+    int arr[n];
+    arr[0] = 0;
+    arr[1] = 1;
+    
+    for(int i = 2; i <= n; ++i) {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    
+    cout << arr[n];
+}
