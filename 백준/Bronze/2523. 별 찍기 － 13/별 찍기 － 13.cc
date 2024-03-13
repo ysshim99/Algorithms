b6fd44c1d7cc2b2c380952b2e32e295e36d89e72
ysshim99 <91107424+ysshim99@users.noreply.github.com>
2024-03-13 19:19:25 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int N;
+    cin >> N;
+
+    for(int i = 1; i <= N; ++i) {
+        for(int j = 0; j < i; ++j) cout << '*';
+        cout << '\n';
+    }
+    for(int i = N-1; i >= 1; --i) {
+        for(int j = 0; j < i; ++j) cout << '*';
+        cout << '\n';
+    }
+}
