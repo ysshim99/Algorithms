9945caeb7d7565c626f6d9a91078f9de4537979d
ysshim99 <91107424+ysshim99@users.noreply.github.com>
2024-03-14 22:47:24 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,44 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define X first
-#define Y second
+#define Y first
+#define X second
 
-int board[51][51];
-bool vis[51][51];
-int dx[4] = { 1,0,-1,0 };
-int dy[4] = { 0,1,0,-1 };
+int board[52][52];
+bool vis[52][52];
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
+
 int m, n, k;
 queue <pair<int, int >> q;
 
-void bfs(int x, int y) {
-  vis[x][y] = true;
-  q.push({ x,y });
-  while (!q.empty()) {
-    auto cur = q.front(); q.pop();
-    for (int dir = 0; dir < 4; dir++) {
-      int nx = cur.X + dx[dir];
-      int ny = cur.Y + dy[dir];
-      if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-      if (vis[nx][ny] || board[nx][ny] != 1) continue;
-      vis[nx][ny] = true;
-      q.push({ nx,ny });
+void bfs(int y, int x) {
+    vis[y][x] = true;
+    q.push({y, x});
+    
+    while(!q.empty()) {
+        auto cur = q.front();
+        q.pop();
+        
+        for(int dir = 0; dir < 4; ++dir) {
+            int ny = cur.Y + dy[dir];
+            int nx = cur.X + dx[dir];
+            
+            if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+            if(vis[ny][nx] || board[ny][nx] != 1) continue;
+            
+            vis[ny][nx] = true;
+            q.push({ny, nx});
+        }
     }
-  }
 }
 
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
+    
   int t;
   cin >> t;
+    
   while (t--) {
     cin >> m >> n >> k;
     int x, y;
+      
     for (int i = 0; i < k; i++) {
       cin >> x >> y;
       board[y][x] = 1;
     }
+      
     int res = 0; //지렁이 개수
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
@@ -48,11 +58,11 @@ int main(void) {
         }
       }
     }
+      
     cout << res << "\n";
     for(int i = 0; i < n; i++){
       fill(board[i], board[i]+m, 0);
       fill(vis[i], vis[i]+m, false);
     }
   }
-  return 0;
 }
