8e8ce0fe0f686dbf5598227ea764c40565de1758
ysshim99 <91107424+ysshim99@users.noreply.github.com>
2024-03-14 22:51:50 +0900

    Update 유기농 배추.cc

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,68 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// pair.first(second) -> pair.Y(X)
 #define Y first
 #define X second
 
 int board[52][52];
-bool vis[52][52];
+bool visited[52][52];
 int dx[4] = {0, 0, 1, -1};
 int dy[4] = {1, -1, 0, 0};
 
-int m, n, k;
-queue <pair<int, int >> q;
+int M, N, K;
+queue<pair<int, int> > Q;
 
 void bfs(int y, int x) {
-    vis[y][x] = true;
-    q.push({y, x});
+    visited[y][x] = true;
+    Q.push({y, x});
     
-    while(!q.empty()) {
-        auto cur = q.front();
-        q.pop();
+    while(!Q.empty()) {
+        auto cur = Q.front();
+        Q.pop();
         
         for(int dir = 0; dir < 4; ++dir) {
             int ny = cur.Y + dy[dir];
             int nx = cur.X + dx[dir];
             
-            if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-            if(vis[ny][nx] || board[ny][nx] != 1) continue;
+            if(ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
+            if(visited[ny][nx] == true || board[ny][nx] == 0) continue;
             
-            vis[ny][nx] = true;
-            q.push({ny, nx});
+            visited[ny][nx] = true;
+            Q.push({ny, nx});
         }
     }
 }
 
 int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     
-  int t;
-  cin >> t;
+    int T;
+    cin >> T;
     
-  while (t--) {
-    cin >> m >> n >> k;
-    int x, y;
-      
-    for (int i = 0; i < k; i++) {
-      cin >> x >> y;
-      board[y][x] = 1;
-    }
-      
-    int res = 0; //지렁이 개수
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        if (board[i][j] == 1 && !vis[i][j]) {
-          bfs(i, j);
-          res++;
+    while(T--) {
+        cin >> M >> N >> K;
+        
+        while(K--) {
+            int X, Y;
+            cin >> X >> Y;
+            board[Y][X] = 1;
         }
-      }
+        
+        int cnt = 0;
+        for(int i = 0; i < N; ++i) {
+            for(int j = 0; j < M; ++j) {
+                if(board[i][j] == 1 && visited[i][j] == false) {
+                    bfs(i, j);
+                    cnt++;
+                }
+            }
+        }
+        cout << cnt << '\n';
+        
+        for(int i = 0; i < N; ++i) {
+            fill(board[i], board[i]+M, 0);
+            fill(visited[i], visited[i]+M, false);
+        }
     }
-      
-    cout << res << "\n";
-    for(int i = 0; i < n; i++){
-      fill(board[i], board[i]+m, 0);
-      fill(vis[i], vis[i]+m, false);
-    }
-  }
 }
