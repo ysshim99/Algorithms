0c70e4579f4e8633661eaa02639c7a72e29042d7
ysshim99 <91107424+ysshim99@users.noreply.github.com>
2024-03-22 19:22:36 +0900

    [Silver III] Title: N과 M (3), Time: 348 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int selected[10];
+
+int N, M;
+
+// start ~ N 중에서 remain개 선택
+void func(int start, int remain) {
+  if(remain == 0) {
+    for(int i = 0; i < M; ++i) {
+      cout << selected[i] << ' ';
+    }
+    cout << '\n';
+    return;
+  }
+
+  for(int i = start; i <= N; ++i) {
+    selected[M-remain] = i;
+    func(1, remain-1);
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> N >> M;
+
+  func(1, M);
+}
