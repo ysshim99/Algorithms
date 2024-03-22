bc802c807f9810eb5e3dc3ff99365780b19bf538
ysshim99 <91107424+ysshim99@users.noreply.github.com>
2024-03-23 07:24:11 +0900

    [Silver III] Title: N과 M (5), Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -7,22 +7,20 @@ int input[10];
 
 int N, M;
 
-// input[start] ~ input[N] 중에서 remain개 선택
+// 현재까지 cnt개 수 선택
 // 입력으로 받은 수는 input[1] ~ input[N]에 저장됨
-void func(int start, int remain) {
-  if(remain == 0) {
-    for(int i = 0; i < M; ++i) {
-      cout << selected[i] << ' ';
-    }
+void func(int cnt) {
+  if(cnt == M) {
+    for(int i = 0; i < M; ++i) cout << selected[i] << ' ';
     cout << '\n';
     return;
   }
 
-  for(int i = start; i <= N; ++i) {
+  for(int i = 1; i <= N; ++i) {
     if(!used[i]) {
-      selected[M-remain] = input[i];
+      selected[cnt] = input[i];
       used[i] = true;
-      func(1, remain-1);
+      func(cnt+1);
       used[i] = false;
     }
   }
@@ -33,11 +31,11 @@ int main(void) {
   cin.tie(0);
 
   cin >> N >> M;
-  
-  for(int i = 0; i < N; ++i) {
-    cin >> input[i+1];
+
+  for(int i = 1; i <= N; ++i) {
+    cin >> input[i];
   }
   sort(input+1, input+1+N);
 
-  func(1, M);
+  func(0);
 }
