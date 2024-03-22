ad33e47b2a00ef0a1a9e61399d6c7f3a694872a2
ysshim99 <91107424+ysshim99@users.noreply.github.com>
2024-03-22 23:26:21 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int selected[10];
+int input[10];
+
+int N, M;
+
+// input[start] ~ input[N] 중에서 remain개 선택
+// 입력으로 받은 수는 input[1] ~ input[N]에 저장됨
+void func(int start, int remain) {
+  if(remain == 0) {
+    for(int i = 0; i < M; ++i) cout << selected[i] << ' ';
+    cout << '\n';
+    return;
+  }
+
+  int prev = -1;
+  for(int i = start; i <= N; ++i) {
+    if(prev != input[i]) {
+      selected[M-remain] = input[i];
+      prev = input[i];
+      func(i, remain-1);
+    }
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> N >> M;
+
+  for(int i = 0; i < N; ++i) cin >> input[i+1];
+  sort(input+1, input+1+N);
+
+  func(1, M);
+}
