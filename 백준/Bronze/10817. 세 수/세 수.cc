ac64ff5dfea85052424412986c535748229ba127
ysshim99 <91107424+ysshim99@users.noreply.github.com>
2024-03-28 01:41:45 +0900

    [Bronze III] Title: 세 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[3];
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  for(int i = 0; i < 3; ++i) cin >> arr[i];
+
+  sort(arr, arr+3);
+  cout << arr[1];
+}
