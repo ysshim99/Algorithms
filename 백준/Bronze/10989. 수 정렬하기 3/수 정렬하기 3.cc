0b6791c77627c09e5f3f769cc25ed963d965ce7c
ysshim99 <91107424+ysshim99@users.noreply.github.com>
2024-04-15 16:20:32 +0900

    Update 수 정렬하기 3.cc

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -1,30 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*
-input을 입력으로 받으면 arr[input] + 1 한다.
-출력할 때는 arr[input]의 값만큼 반복문 수행한다.
-*/
-
 int arr[10002];
 
+int N;
+
 int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int N;
-    cin >> N;
-    
-    while(N--) {
-        int input;
-        cin >> input;
-        
-        arr[input]++;
-    }
-    
-    for(int i = 1; i <= 10000; ++i) {
-        for(int j = 0; j < arr[i]; ++j) {
-            cout << i << '\n';
-        }
-    }
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> N;
+
+  while (N--) {
+    int num;
+    cin >> num;
+
+    arr[num]++;
+  }
+
+  for (int i = 1; i <= 10000; ++i) {
+    while (arr[i]--) cout << i << '\n';
+  }
 }
