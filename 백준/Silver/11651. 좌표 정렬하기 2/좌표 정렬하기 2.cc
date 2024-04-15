b948cf6a766e3459c7bca4385b6e57d8b0af3bf4
ysshim99 <91107424+ysshim99@users.noreply.github.com>
2024-04-15 21:58:06 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 44 ms, Memory: 2680 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -1,22 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-pair<int, int> arr[100002];
+#define X first
+#define Y second
+
 int N;
 
-bool comp(const pair<int, int>& a, const pair<int, int>& b) {
-    if(a.second == b.second) return a.first < b.first;
-    return a.second < b.second;
-}
-
 int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    cin >> N;
-    for(int i = 0; i < N; ++i) cin >> arr[i].first >> arr[i].second;
-    
-    sort(arr, arr+N, comp);
-    
-    for(int i = 0; i < N; ++i) cout << arr[i].first << ' ' << arr[i].second << '\n';
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  cin >> N;
+  
+  pair<int, int> pos[N];
+  for (auto& cur : pos) cin >> cur.Y >> cur.X;
+  
+  sort(pos, pos+N);
+  
+  for (auto cur : pos) cout << cur.Y << ' ' << cur.X << '\n';
 }
