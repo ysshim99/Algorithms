567fc81115caf6622ae0e6feaf58061581739484
ysshim99 <91107424+ysshim99@users.noreply.github.com>
2024-04-15 21:15:09 +0900

    [Silver V] Title: 나이순 정렬, Time: 72 ms, Memory: 7776 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -1,24 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool cmp(pair<int, string> a, pair<int, string> b) {
-    return a.first < b.first;
-}
+int N;
 
 int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int N;
-    cin >> N;
-    
-    vector<pair<int, string> > member(N);
-    
-    for(int i = 0; i < N; ++i)
-        cin >> member[i].first >> member[i].second;
-    
-    stable_sort(member.begin(), member.end(), cmp);
-    
-    for(int i = 0; i < N; ++i) 
-        cout << member[i].first << ' ' << member[i].second << '\n';
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  cin >> N;
+  
+  pair<int, string> user[N];
+  for (auto& cur :  user) cin >> cur.first >> cur.second;
+  
+  stable_sort(user, user+N, [&](pair<int, string> a, pair<int, string> b) {
+    return a.first < b.first;
+  });
+  
+  for (auto cur : user) cout << cur.first << ' ' << cur.second << '\n';
 }
