174fcf44a1f453ff8a88cde17150d094cf7d522f
ysshim99 <91107424+ysshim99@users.noreply.github.com>
2024-04-26 13:44:11 +0900

    Update 가장 긴 증가하는 부분 수열.cc

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 int A[1005];
-int dp[1005];
+int dp[1005]; // 1 ~ i번째 원소에서 가장 긴 부분 수열의 길이. 단, i번째 원소는 반드시 포함해야 됨
 
 int N;
 
@@ -17,7 +17,7 @@ int main(void) {
     dp[i] = 1;
   }
   
-  for (int i = 0; i < N; ++i) {
+  for (int i = 1; i < N; ++i) {
     for (int j = 0; j < i; ++j) {
       if (A[j] < A[i]) dp[i] = max(dp[i], dp[j] + 1);
     }
