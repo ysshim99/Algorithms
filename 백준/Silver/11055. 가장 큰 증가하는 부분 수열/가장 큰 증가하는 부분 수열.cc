8c7309ca7819e245b63b3cde2d7bdd9b98d68004
ysshim99 <91107424+ysshim99@users.noreply.github.com>
2024-04-26 13:41:13 +0900

    Update 가장 큰 증가하는 부분 수열.cc

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -1,25 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
 int A[1005];
 int dp[1005]; // 1 ~ i번째 원소에서 중가하는 부분 수열 중 가장 큰 합. 단, i번째 원소는 반드시 포함해야 됨
+
+int N;
 
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
   cin >> n;
-  for (int i = 0; i < n; ++i) {
+  for (int i = 0; i < N; ++i) {
     cin >> A[i];
     dp[i] = A[i];
   }
       
-  for (int i = 1; i < n; ++i) {
+  for (int i = 1; i < N; ++i) {
     for (int j = 0; j < i; ++j) {
       if (A[j] < A[i]) dp[i] = max(dp[i], dp[j] + A[i]);
     }
   }
 
-  cout << *max_element(dp, dp + n);
+  cout << *max_element(dp, dp+N);
 }
